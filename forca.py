b0d3b387354ad11b65b7aaae281a2b0188abe6e8
Wesley Freire <wesleyfreire1707@gmail.com>
2023-05-01 21:17:58 -0300

    finalizado

--- forca.py
+++ forca.py
@@ -14,3 +14,13 @@ while acertou == False :
     for i in range(0, len(palavra_secreta)):
         if letra == palavra_secreta[i] :
             letras_descobertas[i] = letra
+
+        print(letras_descobertas[i])
+    
+    acertou = True
+
+    for x in range(0, len(letras_descobertas)):
+        if letras_descobertas[x] == "-":
+            acertou = False
+
+print("parabens!")
